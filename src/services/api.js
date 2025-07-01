def83a50f488bafebcfecd6349c8aa35c28c2317
Deepak-Appxcess <deepak@appxcess.com>
2025-07-01 14:03:55 +0530

    api link updated

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://joe-unified-florence-dk.trycloudflare.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
